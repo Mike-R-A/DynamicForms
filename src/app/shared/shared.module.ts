73fe72a0d5a070697ab9bb6820365658950a2442
Ashford M.R <m.r.ashford@swansea.ac.uk>
2019-08-30 11:46:38 +0100

    remove commented imports

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,13 +3,6 @@ import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
-// import { InputTextModule } from 'primeng/inputtext';
-// import { DropdownModule } from 'primeng/dropdown';
-// import { ButtonModule } from 'primeng/button';
-// import { PanelModule } from 'primeng/panel';
-// import { FieldsetModule } from 'primeng/fieldset';
-// import { CardModule } from 'primeng/card';
-// import { SidebarModule } from 'primeng/sidebar';#
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
